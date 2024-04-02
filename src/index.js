6099ef9c88c1087939b207f3a954a181a83fe2ce
blueratdota <jrafaelgocong@gmail.com>
2024-04-02 11:42:41 +0800

    2nd commit

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import './style.css'
 
 console.log('this is index.js');
+console.log('this is index 2.js');
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,34 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
 
 module.exports = {
   mode: 'development',
   entry: './src/index.js',
   devtool: 'inline-source-map',
   output: {
-    filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
   },
+  plugins: [new HtmlWebpackPlugin({
+    template: './src/index.html',
+    filename: 'index.html',
+    inject: 'body'
+  }),
+        new CopyWebpackPlugin({'patterns': [
+          { from: './src/images', to: 'images' }
+        ]})
+  ],
   module: {
     rules: [
       {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.html$/i,
+        loader: "html-loader",
       },
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
